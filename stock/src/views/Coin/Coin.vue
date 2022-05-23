c3c961d6090a1612ac16f50238789a9652d5c3ba
v215018 <v215018@cusmo.cup>
2022-05-23 17:37:40 +0900

    텍스트/ 코인리스트/ 스타일 수정

--- stock/src/views/Coin/Coin.vue
+++ stock/src/views/Coin/Coin.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <h1>Coin Home</h1>
+    <h1>Coin Home(UPBIT API)</h1>
     <ul>
       <li v-for="(value, key, index) in responseData" :key="index">
         <div>
           <span class="coin">{{ key }}</span>
+          <span class="koreanName">{{ value.koreanName }}</span>
           <span
             class="indices"
             :class="{
@@ -26,9 +27,7 @@
             }"
             >{{ value.trade_price | comma }}</span
           >
-          <span class="start"
-            >금일 시작가 : {{ value.opening_price | comma }}</span
-          >
+          <span class="start">시작가 : {{ value.opening_price | comma }}</span>
           <span class="average"
             >52주 : {{ value.highest_52_week_price | comma }} /
             {{ value.lowest_52_week_price | comma }}</span
@@ -48,7 +47,18 @@ export default {
     return {
       websocket: null,
       socket: null,
-      requestCoinList: ["KRW-BTC", "KRW-ETH", "KRW-ETC", "KRW-XRP", "KRW-BTT"],
+      requestCoinList: [
+        "KRW-BTC",
+        "KRW-ETH",
+        "KRW-ETC",
+        "KRW-XRP",
+        "KRW-ADA",
+        "KRW-STEEM",
+        "KRW-DOGE",
+        "KRW-MLK",
+        "KRW-OMG",
+        "KRW-XLM",
+      ],
       responseData: [],
       message: null,
       requestList: [],
@@ -89,6 +99,7 @@ export default {
 
         if (forSplit[0] === "code" && this.pear[forSplit[1]]) {
           forObject["pear"] = this.pear[forSplit[1]].stage;
+          forObject["koreanName"] = this.pear[forSplit[1]].korean_name;
         }
       });
       this.responseData[forObject.code] = forObject;
@@ -173,27 +184,39 @@ export default {
   padding: 0;
 }
 h1 {
+  margin: 30px 0 0;
   text-align: center;
 }
 ul {
   display: flex;
   flex-flow: row wrap;
-  margin: 20px 0 0;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  margin: 10px 0 0;
 }
 li {
   display: flex;
   width: 320px;
-  margin: 20px auto 0;
+  margin: 10px auto 0;
+  padding: 15px;
   list-style: none;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-sizing: border-box;
+  box-shadow: 5px 4px 25px -4px #ddd;
+}
+li > div:last-of-type {
+  margin: 0 0 0 auto;
 }
 .coin {
   display: inline-block;
   margin: 0 10px 0 0;
-  font-size: 20px;
+  font-size: 16px;
   font-weight: bold;
 }
-li > div:last-of-type {
-  margin: 0 0 0 auto;
+.koreanName {
+  display: block;
+  font-size: 14px;
 }
 .indices {
   display: block;
@@ -232,12 +255,12 @@ li > div:last-of-type {
 li .start {
   display: block;
   margin: 5px 0 0;
-  font-size: 14px;
+  font-size: 13px;
   text-align: right;
 }
 li .average {
   display: block;
-  font-size: 15px;
+  font-size: 14px;
   text-align: right;
 }
 @media (min-width: 640px) {
